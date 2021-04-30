0ee084f820927d33d09a4a8028e135fd4d50e800
mgmer <disbesean@gmail.com>
2021-04-30 08:50:50 -0400

    updated backend server path

--- server.js
+++ server.js
@@ -15,12 +15,16 @@ app.use(cors());
 app.use(express.json());
 app.use("/api",activityRouter);
 
-if (process.env.Node_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    }); 
-}
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 app.listen(port, host, async () => {
     console.log(`Server is running on port ${port}`);
