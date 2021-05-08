4d135fb83b32525066c3034ee3f52c37439bea61
mgmer <disbesean>
2021-05-08 18:36:42 -0400

    updated strava-redirect link

--- client/src/Components/Authentication/StravaAuth.js
+++ client/src/Components/Authentication/StravaAuth.js
@@ -3,7 +3,7 @@ import React from 'react';
 function StravaAuth() {
     const scope = 'read,activity:read_all';
     const handleLogin = () => {
-        window.location = `http://www.strava.com/oauth/authorize?client_id=64006&response_type=code&redirect_uri=${process.env.REACT_APP_URL}/strava-redirect/exchange_token&approval_prompt=force&scope=${scope}`;
+        window.location = `http://www.strava.com/oauth/authorize?client_id=64006&response_type=code&redirect_uri=${process.env.REACT_APP_URL}/strava-redirect&approval_prompt=force&scope=${scope}`;
     };
 
     return (
